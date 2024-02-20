64fefd64a0383a15438b6fc1b10c2e2f828fa505
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-02-20 07:50:23 +0700

    fix: set home url for user guest and authorized

--- controllers/LayoutController.php
+++ controllers/LayoutController.php
@@ -8,19 +8,28 @@ use app\helpers\Utils;
 use app\models\Log;
 use app\models\User;
 use Yii;
+use yii\helpers\Url;
 use yii\web\Controller;
 
-class LayoutController extends Controller {
+class LayoutController extends Controller
+{
+    public function behaviors()
+    {
+        return AuthHelpers::behaviors();
+    }
     public function beforeAction($action)
     {
+        $user = Yii::$app->user;
+
         if (!parent::beforeAction($action)) {
             return false;
         }
-        $user = Yii::$app->user;
         // echo '<pre>';print_r($user->identity->role);die;
         if ($user->isGuest) {
             $this->layout = 'main';
-        } elseif (in_array($user->identity->role->item_name, [Utils::ROLE_ADMIN,Utils::ROLE_EDITOR,Utils::ROLE_AUTHOR]) ) {
+            Yii::$app->setHomeUrl(Url::to(['site']));
+        } elseif (in_array(User::me()->role->item_name, [Utils::ROLE_ADMIN, Utils::ROLE_EDITOR, Utils::ROLE_AUTHOR])) {
+            Yii::$app->setHomeUrl(Url::to(['/dashboard']));
             $this->layout = 'main-lte';
         }
 
