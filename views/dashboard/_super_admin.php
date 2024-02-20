432eb95e8a053252123ab683abcaf279b9508024
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-02-20 07:54:09 +0700

    style: recolor dashboard bg wrapper

--- views/dashboard/_super_admin.php
+++ views/dashboard/_super_admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use app\models\User;
-
+$this->title = 'Dashboard';
 ?>
 <h3 class="text-white fw-800 mb-0">👋 Hi <?= User::me()->username; ?>,</h3>
 <small class="text-white ">Welcome back. Enjoy managing your platform efficiently.</small>
@@ -11,11 +11,7 @@ use app\models\User;
         display: block;
     }
 
-    .btn-main {
-        background: #f17f4663;
-        color: #f17f46;
-        border: none;
-    }
+
 
     .card-dashboard {
         border-radius: 8px;
@@ -24,25 +20,25 @@ use app\models\User;
 <div class="row">
     <div class="col">
         <div class="card card-body card-dashboard p-3">
-            <small class="text-muted"><button class="btn btn-main fa-solid fa-user mr-1"></button> User Active</small>
+            <small class="text-muted"><button class="btn btn-main-secondary fa-solid fa-user mr-1"></button> User Active</small>
             <h4 class="mt-2">81</h4>
         </div>
     </div>
     <div class="col">
         <div class="card card-body card-dashboard p-3">
-            <small class="text-muted"><button class="btn btn-main fa-solid fa-user mr-1"></button> User Active</small>
+            <small class="text-muted"><button class="btn btn-main-secondary fa-solid fa-user mr-1"></button> User Active</small>
             <h4 class="mt-2">81</h4>
         </div>
     </div>
     <div class="col">
         <div class="card card-body card-dashboard p-3">
-            <small class="text-muted"><button class="btn btn-main fa-solid fa-user mr-1"></button> User Active</small>
+            <small class="text-muted"><button class="btn btn-main-secondary fa-solid fa-user mr-1"></button> User Active</small>
             <h4 class="mt-2">81</h4>
         </div>
     </div>
     <div class="col">
         <div class="card card-body card-dashboard p-3">
-            <small class="text-muted"><button class="btn btn-main fa-solid fa-user mr-1"></button> User Active</small>
+            <small class="text-muted"><button class="btn btn-main-secondary fa-solid fa-user mr-1"></button> User Active</small>
             <h4 class="mt-2">81</h4>
         </div>
     </div>
